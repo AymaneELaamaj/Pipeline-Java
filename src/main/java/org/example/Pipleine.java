fedf3a46f1c95de39529a676181a372d237928e2
AymaneElaamaj <aymaneamg300@gmail.com>
2025-04-06 15:57:35 +0100

    add Pipleine Class

--- src/main/java/org/example/Pipleine.java
+++ src/main/java/org/example/Pipleine.java
@@ -1,2 +1,23 @@
-package org.example;public class Pipleine {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pipleine {
+    List<OrderProcessing> stages=new ArrayList<>();
+    public Pipleine addstage(OrderProcessing stage){
+        stages.add(stage);
+        return this;
+    }
+    public Order execute(Order order){
+        for (OrderProcessing stage:stages){
+            order=stage.process(order);
+            if ("Invalid".equals(order.Statut) || "Failed".equals(order.Statut)){
+                break;
+            }
+
+
+        }
+        return order;
+    }
 }
